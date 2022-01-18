7b1ea1d23ad3cc85084c49ef0a0ea0c704403858
IsaacDilley <97974909+IsaacDilley@users.noreply.github.com>
2022-01-18 13:12:10 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+
+print("Ayo the pizza here")
+time.sleep(2)
+print("*slip sfx*")
+time.sleep(1.5)
+print("AGGH")
+time.sleep(1.5)
+print("My ears burn")
+
+doesyours = str(input('Does your ears burn?\n'))
+if doesyours == "yes":
+    print('same bro')
+elif doesyours == "no":
+    print('NOT HALLAL VERY HARAM 😡😡😡😡')
+else:
+    print('bro tf you talking about 💀')
