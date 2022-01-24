9a845ea22fa9f90f730f94719edd1ec5b31c624b
IsaacDilley <97974909+IsaacDilley@users.noreply.github.com>
2022-01-24 11:29:26 -0600

    Update myearsburn.py
    added some "features"

--- myearsburn.py
+++ myearsburn.py
@@ -1,5 +1,6 @@
-print('Running MEB v1.23 | Made by Isaac Dilley')
+print('Running MEB v1.4 | Made by Isaac Dilley')
 import time
+import random
 
 print("Ayo the pizza here")
 time.sleep(2)
@@ -14,23 +15,39 @@ if doesyours == "yes":
     print('same bro')
 elif doesyours == "no":
     print('NOT HALLAL VERY HARAM 😡😡😡😡')
+     ending = random.randint(100000, 999999)
+    allahbadf = open(f'youarebadperson{ending}.txt', 'w')
+    allahbadf.write('I AMOG OING TO BAN yoU OFRM GAME FOR THAT ALALH IS BAEST :mad_face: :mad_face: :mad_face: :mad_face: >:(')
+    allahbadf.close()
 else:
     print('bro tf you talking about 💀')
     
 isharamgood = str(input('is haram good?\n'))
 if isharamgood == 'yes':
     print('NOT HALLAL VERY HARAM 😡😡😡😡')
+    ending = random.randint(100000, 999999)
+    allahbadf = open(f'youarebadperson{ending}.txt', 'w')
+    allahbadf.write('I AMOG OING TO BAN yoU OFRM GAME FOR THAT ALALH IS BAEST :mad_face: :mad_face: :mad_face: :mad_face: >:(')
+    allahbadf.close()
 elif isharamgood == 'no':
     print('very hallal 😌😌😌 allah best 🥱')
 else:
     print('😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡')
+    ending = random.randint(100000, 999999)
+    allahbadf = open(f'youarebadperson{ending}.txt', 'w')
+    allahbadf.write('I AMOG OING TO BAN yoU OFRM GAME FOR THAT ALALH IS BAEST :mad_face: :mad_face: :mad_face: :mad_face: >:(')
+    allahbadf.close()
     time.sleep(5)
 
 isallahbad = str(input('is allah bad?\n'))
 if isallahbad == 'yes':
     print('😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡')
+    ending = random.randint(100000, 999999)
+    allahbadf = open(f'youarebadperson{ending}.txt', 'w')
+    allahbadf.write('I AMOG OING TO BAN yoU OFRM GAME FOR THAT ALALH IS BAEST :mad_face: :mad_face: :mad_face: :mad_face: >:(')
+    allahbadf.close()
     time.sleep(5)
-elif isallahbad == 'yes':
+elif isallahbad == 'no':
     print('allah best 🤤🤤🤤🤤🤤')
 else:
     print('what are you saying lol')
